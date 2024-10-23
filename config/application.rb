65ff6a38b76ed531e507e96bee9efd1b9067a701
hasonsk <sonhh2003@gmail.com>
2024-10-23 21:24:19 +0700

    Chapter_3_4_5
    Chapter 4 | Rails-Flavored Ruby - yield
    
    Chapter_3_4_5

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module RailsTutorial
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.i18n.default_locale = :vi
+    config.i18n.available_locales = [:en, :vi, :ja]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -18,5 +19,8 @@ module RailsTutorial
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    Config.setup do |config|
+      config.const_name = "Settings" # This is the default
+    end
   end
 end
