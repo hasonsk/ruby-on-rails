9e348617e7cb5f03447f9e87b3a487a029ec51a7
hasonsk <sonhh2003@gmail.com>
2024-10-31 16:40:10 +0700

    Chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   before_save{self.email = email.downcase}
   validates :name, presence: true, length: {maximum: Settings.default.name_user_max_length}
   validates :email, presence: true, length: {maximum: Settings.default.email_user_max_length},
-                    format: {with: Rails.application.config.email_regex },
+                    format: {with: Rails.application.config.email_regex},
                     uniqueness: true
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, presence: true, length: {minimum: Settings.default.password_min_length}
   has_secure_password
 end
