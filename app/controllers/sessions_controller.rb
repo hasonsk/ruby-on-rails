8fcd1d8bd943c3a3a42243ed73345490457e6362
hasonsk <sonhh2003@gmail.com>
2024-11-06 08:22:18 +0700

    Chapter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,10 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate(params[:session][:password])
       reset_session
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
       log_in user
       redirect_to user
     else
@@ -14,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url, status: :see_other
   end
 end
--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,10 +1,10 @@
 // Menu manipulation
 // Adds a toggle listener.
 function addToggleListener(selected_id, menu_id, toggle_class) {
-  let selected_element = document.querySelector('#${selected_id}');
-  selected_element.addEventListener('click', function(event) {
+  let selected_element = document.querySelector(`#${selected_id}`);
+  selected_element.addEventListener(`click`, function(event) {
     event.preventDefault();
-    let menu = document.querySelector('#${menu_id}')
+    let menu = document.querySelector(`#${menu_id}`)
     menu.classList.toggle(toggle_class);
   });
 }
